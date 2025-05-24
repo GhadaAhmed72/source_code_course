b2390066dcb922c9236963fe57ce42690f5c9aa5
Ghada Ahmed <88281680+GhadaAhmed72@users.noreply.github.com>
2025-05-24 13:41:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let alertButton = document.getElementById("alert");
+alertButton.addEventListener("click" , function(){
+    // alert("Hello world");
+    alert("welcome to source control course");
+    
+})
+
